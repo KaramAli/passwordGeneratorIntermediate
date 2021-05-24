6cd6172353c15ffc17835bf37b1dbf3063c1d3b3
Karam Ali <karam.13@hotmail.co.uk>
2021-05-24 13:35:17 +0100

    started project

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+print("Welcome to the Advanced Password Generator!")
+
+num_of_letters = int(input("How many letters would you like in you password?"))
+num_of_symbols = int(input("How many symbols would you like in you password?"))
+num_of_letters = int(input("How many numbers would you like in you password?"))
+
